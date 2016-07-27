bd3441b600b5792c3fcaf832c5fed3ef17f8b556
Liam Bennett <liam@littleowllabs.com>
2016-07-27 14:03:54 +0100

    feat: prioritise by follower count

--- twitter/driver.rb
+++ twitter/driver.rb
@@ -17,10 +17,22 @@ class Twitter
     search_page.load
   end
 
+  def followers(stream_item)
+    stream_item.user_profile_link.hover
+    search_page.wait_for_follower_count
+    search_page.follower_count.text
+  end
+
   def post_reply(last)
-    stream_item = search_page.stream_items.first
-    id = stream_item.root_element['data-item-id']
-    if id != last
+    stream_items = search_page.stream_items.reject do |stream_item|
+      id <= stream_item.root_element['data-item-id']
+    end
+
+    stream_item = stream_items.max do |a,b|
+      followers(a) <=> followers(b) 
+    end
+    
+    if stream_item
       username = "@#{stream_item.username.text}"
       stream_item.reply_button.click
 
--- twitter/pages/search_page.rb
+++ twitter/pages/search_page.rb
@@ -3,9 +3,12 @@ module TwitterPages
     set_url 'https://twitter.com/search?f=tweets&vertical=default&q=%22Your%20welcome%22&src=typd'
 
     sections :stream_items, 'ol.stream-items li.stream-item' do
+      element :user_profile_link, '.account-group.js-user-profile-link'
       element :username, '.account-group .username b'
       element :reply_button, '.ProfileTweet-actionButton.js-actionReply'
     end
+
+    element :follower_count, '.ProfileCardStats-statValue'
 
     element :editor, '.tweet-box.rich-editor'
     element :submit_button, '.modal-tweet-form-container .tweet-btn'
